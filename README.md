e9d4eeb74612d7300da8040b1d4ceb418c3a48f7
feeloho <79534505@qq.com>
2021-10-18 10:02:16 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Actions-OpenWrt
+
+使用[P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)模板编译，感谢~
+
+
+## 1.0.0
+-  精简初始包
+## License
+
+[MIT](https://github.com/P3TERX/Actions-OpenWrt/blob/main/LICENSE) © [**feeloho**](https://github.com/feeloho)
---
+++ diy-part1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
+git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config  #argon主题设置（编译时候选上,在固件的‘系统’里面）
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
+git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
+git clone https://github.com/small-5/luci-app-adblock-plus package/lean/luci-app-adblock-plus  #adblock plus+ 去广告
+git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/lean/luci-app-v2ray #v2ray
