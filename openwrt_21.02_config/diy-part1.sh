8e6e7f7622b006ebb24e8356b3655ce5e9e48565
feeloho <79534505@qq.com>
2021-11-13 22:33:55 +0800

    1.0.0

--- openwrt_21.02_config/diy-part1.sh
+++ openwrt_21.02_config/diy-part1.sh
@@ -14,10 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #新的argon主题
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/kenzok8/openwrt-packages.git package/kenzo
+git clone https://github.com/kenzok8/small.git package/small
